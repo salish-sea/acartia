55d7d73e371fda94592c441554774d9bbe1bf5a8
Tom Hollo <shimtucker22187@hotmail.com>
2026-08-10 22:18:47 +1000

    move api to single openapi.yml file

--- backend/src/main/java/acartia/sightings/Species.java
+++ backend/src/main/java/acartia/sightings/Species.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
